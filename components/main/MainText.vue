b59af6ce51c69edbe7d7a3d6ffc0a692ada9ecc5
insplanet_이희원 <hw.lee@insplanet.co.kr>
2022-05-26 16:29:47 +0900

    feat: main text component 생성

---
+++ components/main/MainText.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="txt-wrap">
+    <span class="category">{{ category }}</span>
+    <strong class="title">{{ title }}</strong>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainText',
+  data() {
+    return {
+      category: 'Application',
+      title: '서비스 접근성 확대와 트렌드를 녹인\n신한플러스 메인 개편',
+    }
+  },
+}
+</script>
