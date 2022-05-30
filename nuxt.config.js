349ec8b89ec032e5020155c188261b907a2a1c7c
insplanet_이희원 <hw.lee@insplanet.co.kr>
2022-05-30 17:36:02 +0900

    fix: nuxt config 파일에 누락된 정보 기입

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,9 +34,10 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/style-resources',
   ],
   styleResources: {
-    scss: ['@assets/scss/_variables.scss'],
+    scss: ['./assets/scss/_variables.scss'],
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
