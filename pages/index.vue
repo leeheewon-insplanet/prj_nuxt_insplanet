bf8854bcf24fd3c66e86dd6a856d19ce21276034
insplanet_이희원 <hw.lee@insplanet.co.kr>
2022-06-07 17:33:14 +0900

    fix: 임시로 이미지 import 방식 설정

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,22 @@
 <template>
   <!-- START : 메인 페이지 -->
   <div id="wrapper" class="main">
-    <div class="list-item" :style="'background-image: ' + work.bgImg">
+    <div class="list-item" :style="{ backgroundImage: `url(${MainImage})` }">
       <MainText />
     </div>
   </div>
 </template>
 
 <script>
+import MainImage from '@/assets/images/img/img_project17_head.png'
 import MainText from '@/components/main/MainText.vue'
+
 export default {
   name: 'MainPage',
   components: { MainText },
   data() {
     return {
-      work: [
-        {
-          bgImg: '"../assets/images/img/img_project17_head.png"',
-        },
-      ],
+      MainImage,
     }
   },
 }
