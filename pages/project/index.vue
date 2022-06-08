365d8ad2bf4b6ddd44273be443fc95519aa2b7a9
insplanet_이희원 <hw.lee@insplanet.co.kr>
2022-06-08 17:28:42 +0900

    feat: 서브메뉴 페이지 생성

---
+++ pages/project/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- START : Project 페이지 -->
+  <div id="wrapper" class="project">project page입니다.</div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectPage',
+}
+</script>
