cb13f3303d8548e3aafe95a078f705a10bae3628
insplanet_이희원 <hw.lee@insplanet.co.kr>
2022-06-22 17:56:47 +0900

    fix: swiper import 방식 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '@/plugins/vue-awesome-swiper', mode: 'client' }],
+  plugins: [{ src: '@/plugins/swiper.js', ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
