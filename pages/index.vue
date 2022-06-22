61e26486227019e06d3b464ae3fe9a924ece40c3
insplanet_이희원 <hw.lee@insplanet.co.kr>
2022-06-22 17:56:56 +0900

    fix: swiper 기능 구현중

--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,76 @@
 <template>
   <!-- START : 메인 페이지 -->
   <div id="wrapper" class="main">
-    <div
-      v-swiper="swiperOption"
-      class="w-5/6 ml-auto relative"
-      :loadtheme="false"
-    >
+    <div v-swiper:mySwiper="swiperOption" class="swiper-container">
       <div class="swiper-wrapper">
-        <div class="swiper-slide" :key="banner" v-for="banner in banners">
-          <!-- Render original HTML in server, render Swiper in browser (client) -->
-          <img class="h-64 w-64" :src="banner" />
+        <div
+          v-for="(slide, index) in slides"
+          :key="index"
+          class="swiper-slide list-item"
+          :style="{ backgroundImage: `url(${MainImage})` }"
+        >
+          <h2>{{ slide.name }}</h2>
+          <MainText />
         </div>
       </div>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-    </div>
-    <div class="list-item" :style="{ backgroundImage: `url(${MainImage})` }">
-      <MainText />
+
+      <!-- navigation -->
+      <div slot="button-prev" class="swiper-button-prev swiper-btn-prev"></div>
+      <div slot="button-next" class="swiper-button-next swiper-btn-next"></div>
+
+      <!-- pagination -->
+      <!-- <div v-if="slides.length > 1" class="swiper-pagination"></div> -->
     </div>
   </div>
 </template>
 
 <script>
-import { directive } from 'vue-awesome-swiper'
 import MainImage from '@/assets/images/img/img_project17_head.png'
 import MainText from '@/components/main/MainText.vue'
 
 export default {
   name: 'MainPage',
-  directives: {
-    swiper: directive,
-  },
   components: { MainText },
   data() {
     return {
       MainImage,
-      banners: ['/slider-1.png', '/slider-2.png', '/slider-3.png'],
-      swiperOption: {
-        slidesPerView: 3,
-        spaceBetween: -10,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+      slides: [
+        {
+          name: '1',
         },
+        {
+          name: '2',
+        },
+        {
+          name: '3',
+        },
+      ],
+      swiperOption: {
+        loop: true,
+        slidesPerView: 2,
+        // pagination: {
+        //   el: '.swiper-pagination',
+        //   clickable: true,
+        //   dynamicBullets: true,
+        //   dynamicMainBullets: 2,
+        // },
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
         breakpoints: {
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 10,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 10,
-          },
-          640: {
+          // when window width is >= 480px
+          480: {
             slidesPerView: 2,
             spaceBetween: 10,
           },
-          320: {
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1264: {
             slidesPerView: 1,
-            spaceBetween: 10,
           },
         },
       },
